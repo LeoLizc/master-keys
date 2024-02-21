78a62ab54fedada7b4c1a1b02529036038e7b96b
LeoLizc <leolizcanop@gmail.com>
2024-02-21 15:54:04 -0500

    Add MasterKeys component

---
+++ src/master-keys.ts
@@ -0,0 +1,33 @@
+export class MasterKeys extends HTMLElement {
+  placeholder = 'Type a command or search...';
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot!.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          padding: 16px;
+          font-family: sans-serif;
+        }
+        input {
+          width: 100%;
+          padding: 8px;
+          font-size: 16px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          box-sizing: border-box;
+        }
+      </style>
+      <input type="text" placeholder="${this.placeholder}">
+    `;
+  }
+}
